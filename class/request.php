8ad9ccffc6361099c47faa84bf6219701a2c3674
DevMD963 <marksoriza@gmail.com>
2023-02-09 12:38:42 -0800

    new api Update

--- class/request.php
+++ class/request.php
@@ -1,23 +1,31 @@
 <?php
 require '../vendor/autoload.php';
-use Curl\Curl;
-$curl = new Curl();
-$curl->setHeader('Host','jrustonapps.net'); 
-$curl->setHeader('user-agent','okhttp/3.14.9'); 
-$curl->setHeader('app-request-time','1675695817'); 
-$curl->setHeader('app-user-id','MEAA-71176A2D-FD07-4DCF-88E9-614CD0799B67'); 
-$curl->get('https://jrustonapps.net/app-apis/earthquakes/get-recent.php');
-$res = $curl->response;
+class info {
+public function inf() :self {
+(new \Curl\Curl())->setHeader('Host','jrustonapps.net'); 
+(new \Curl\Curl())->setHeader('user-agent','okhttp/3.14.9'); 
+(new \Curl\Curl())->setHeader('app-request-time','1675695817'); 
+(new \Curl\Curl())->setHeader('app-user-id','MEAA-71176A2D-FD07-4DCF-88E9-614CD0799B67'); 
+(new \Curl\Curl())->get('https://jrustonapps.net/app-apis/earthquakes/get-recent.php');
+$res = (new \Curl\Curl())->response;
 $re = str_replace(["<--STARTOFCONTENT-->","<--ENDOFCONTENT-->"],'',$res);
-$rec = json_decode($re)->earthquakes;
-foreach ($rec as $rc){
+return $re;
+/**$rec = json_decode($re)->earthquakes;
+print_r($rec);**/
+}
+   }
+$renco = new info;
+echo $renco->inf();
+
+/**foreach ($rec as $rc){
 	$rt[] = array("id"=>$rc->id,"time"=>$rc->time,"latitude"=>$rc->latitude,"longitude"=>$rc->longitude,"country"=>$rc->country,"depth"=>$rc->depth,"place"=>$rc->place,"continent"=>$rc->continent,"mag"=>$rc->mag,"location"=>$rc->location);
 	}
 	
 	$tr = json_encode($rt,128|64|256);
-	echo $tr;
+	echo $tr;**/
 	
-if ($curl->error) {
-    echo 'Error: ' . $curl->errorMessage . "\n";
-    $curl->diagnose();
+	
+if ((new \Curl\Curl())->error) {
+    echo 'Error: ' . (new \Curl\Curl())->errorMessage . "\n";
+    (new \Curl\Curl())->diagnose();
 }
