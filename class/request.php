6fbacb8c4f00296b75c48950d81727edefb21160
DevMD963 <marksoriza@gmail.com>
2023-02-09 07:11:18 -0800

    api update

--- class/request.php
+++ class/request.php
@@ -8,7 +8,7 @@ $curl->setHeader('app-request-time','1675695817');
 $curl->setHeader('app-user-id','MEAA-71176A2D-FD07-4DCF-88E9-614CD0799B67'); 
 $curl->get('https://jrustonapps.net/app-apis/earthquakes/get-recent.php');
 $res = $curl->response;
-$re = str_replace(['<--STARTOFCONTENT-->','<--ENDOFCONTENT-->'],$res);
+$re = str_replace(["<--STARTOFCONTENT-->","<--ENDOFCONTENT-->"],'',$res);
 echo $re;
 /**class database extends \YhyaSyrian\Sql\SyDb {
     public function addData(string $result) :self {
@@ -20,6 +20,4 @@ echo $re;
 if ($curl->error) {
     echo 'Error: ' . $curl->errorMessage . "\n";
     $curl->diagnose();
-} else {
-    echo $curl->response;
 }
