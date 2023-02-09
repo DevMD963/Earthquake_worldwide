bca4e16d5c4f6fa64c8f8b9b780b8c7b3eec573a
DevMD963 <marksoriza@gmail.com>
2023-02-09 15:10:33 -0800

    all done

--- class/request.php
+++ class/request.php
@@ -1,31 +1,13 @@
 <?php
-require '../vendor/autoload.php';
 class info {
-public function inf() :self {
-(new \Curl\Curl())->setHeader('Host','jrustonapps.net'); 
-(new \Curl\Curl())->setHeader('user-agent','okhttp/3.14.9'); 
-(new \Curl\Curl())->setHeader('app-request-time','1675695817'); 
-(new \Curl\Curl())->setHeader('app-user-id','MEAA-71176A2D-FD07-4DCF-88E9-614CD0799B67'); 
-(new \Curl\Curl())->get('https://jrustonapps.net/app-apis/earthquakes/get-recent.php');
-$res = (new \Curl\Curl())->response;
-$re = str_replace(["<--STARTOFCONTENT-->","<--ENDOFCONTENT-->"],'',$res);
-return $re;
-/**$rec = json_decode($re)->earthquakes;
-print_r($rec);**/
-}
-   }
-$renco = new info;
-echo $renco->inf();
-
-/**foreach ($rec as $rc){
-	$rt[] = array("id"=>$rc->id,"time"=>$rc->time,"latitude"=>$rc->latitude,"longitude"=>$rc->longitude,"country"=>$rc->country,"depth"=>$rc->depth,"place"=>$rc->place,"continent"=>$rc->continent,"mag"=>$rc->mag,"location"=>$rc->location);
-	}
-	
-	$tr = json_encode($rt,128|64|256);
-	echo $tr;**/
-	
-	
-if ((new \Curl\Curl())->error) {
-    echo 'Error: ' . (new \Curl\Curl())->errorMessage . "\n";
-    (new \Curl\Curl())->diagnose();
+	public function all(){
+		$earth = file_get_contents('https://earthquake-update.com/earthqu.php');
+echo $earth;
+		}
+		
+		public function arabic(){
+		$earth = file_get_contents('https://earthquake-update.com/earthqu_ar.php');
+echo $earth;
+		}
+		
 }
