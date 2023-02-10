01f4585943952091c44dd5199661434d0e62bf37
DevMD963 <marksoriza@gmail.com>
2023-02-10 01:17:07 -0800

    install.sh created

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo apt update && chmod 777 * && sudo apt upgrade && sudo apt-get install php php-mysql php-zip php-curl php-mbstring php-json php-cli php-dev && sudo apt install figlet && sudo apt install wget && sudo apt install composer && composer i
